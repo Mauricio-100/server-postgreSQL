c78ed47abb4e5ce0c08dec613cf41a950221109c
Mauricio-100 <ceoseshell@gmail.com>
2025-09-01 20:15:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-// index.js
 import express from "express";
 import mysql from "mysql2/promise";
-import fs from "fs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Charger le certificat SSL
-const ca = fs.readFileSync("./ca.pem", "utf8");
-
-// Créer pool MySQL
+// Connexion via pool MySQL
 const pool = mysql.createPool({
-  host: "mysql-1a36101-botwii.c.aivencloud.com",
-  port: 14721,
-  user: "avnadmin",
-  password: "AVNS_BvVULOCxM7CcMQd0Aqw",
-  database: "defaultdb",
-  ssl: { ca }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { ca: process.env.DB_SSL_CERT }
 });
 
-// Exemple route API
 app.get("/", async (req, res) => {
   const [rows] = await pool.query("SELECT username, email FROM users LIMIT 5");
   res.json(rows);
